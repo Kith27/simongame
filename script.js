b21c761ed289a17faa2e3e97a59d716b181439e5
Kith3897 <91460970+Kith27@users.noreply.github.com>
2021-11-24 09:46:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var basicColors = ["green", "red", "yellow", "blue" ]
+var realPattern = []
+var userPattern = []
+var game = false
+var level = 1
+currentLevel = 0
+
+$(document).keydown(function(){
+    if (game === false){
+        gameKey()
+        game = true
+    }
+})
+
+$(".btn").click(function(){
+    var buttonClicked = this.id
+    $("#"+buttonClicked).addClass("pressed")
+    playSound(buttonClicked)
+    setTimeout(function() {
+        $("#"+buttonClicked).removeClass("pressed")
+    }, 100)
+    userPattern.push(buttonClicked)
+    console.log("user "+userPattern)
+    console.log("level " + currentLevel)
+    currentLevel++
+    checkAnswer()
+
+    
+    
+})
+
+function checkAnswer(){
+    if (userPattern[currentLevel-1] === realPattern[currentLevel-1]){
+        if (userPattern.length === realPattern.length){
+            level++
+            setTimeout(gameKey,1000)
+            currentLevel = 0
+        }
+    }else{
+        playSound("wrong")
+        $("h1").text("Game over. Press any key to restart")
+        game = false
+        level = 1
+    }
+}
+
+
+function gameKey(){
+    $("h1").text("Level "+level)
+    userPattern = []
+    var randomNumber = Math.floor(Math.random()*4)
+    var randomColor = basicColors[randomNumber]
+    realPattern.push(randomColor)
+
+    $("."+randomColor).fadeOut(100).fadeIn(100)
+    playSound(randomColor)
+}
+
+function playSound(name) {
+    new Audio("Sounds/"+ name + ".mp3").play()
+    console.log("real "+realPattern)
+}
